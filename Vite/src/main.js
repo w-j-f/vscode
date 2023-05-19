ae40e767e398b8f344466a04e2c77a3f97005285
WJFENGye <806612382@qq.com>
2023-05-19 16:02:58 +0800

    nc

--- Vite/src/main.js
+++ Vite/src/main.js
@@ -1,5 +1,9 @@
+/*
+ * @name: WJQING
+ * @Date: 2023-05-19 15:57:34
+ * @LastEditTime: 2023-05-19 16:02:20
+ */
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
